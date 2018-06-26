e5bb5932af748b65fa77370b32bcf98cac6f9d05
Jonas Grimfelt <grimen@gmail.com>
2018-06-26 01:41:28 -0400

    compat: minimal Python 2 vs 3 helper

---
+++ config2/compat.py
@@ -0,0 +1,41 @@
+
+# =========================================
+#       DEPS
+# --------------------------------------
+
+from easypackage.syspath import syspath
+
+syspath()
+
+# @see https://github.com/benjaminp/six/blob/master/six.py
+
+import sys
+import types
+
+
+# =========================================
+#       CONSTANTS
+# --------------------------------------
+
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
+PY34 = sys.version_info[0:2] >= (3, 4)
+
+
+# =========================================
+#       VARIABLES
+# --------------------------------------
+
+if PY3:
+    string_types = str,
+    integer_types = int,
+    class_types = type,
+    text_type = str
+    binary_type = bytes
+
+else:
+    string_types = basestring,
+    integer_types = (int, long)
+    class_types = (type, types.ClassType)
+    text_type = unicode
+    binary_type = str
