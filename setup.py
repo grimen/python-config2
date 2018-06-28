ef534adce5a87994ecd1209d464d7c02d96b59ef
Jonas Grimfelt <grimen@gmail.com>
2018-06-28 17:42:02 -0400

    version: v0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name = 'config2',
-    version = '0.1.0',
+    version = '0.1.1',
     description = (
         'Python application configuration - highly inspired by `node-config`.'
     ),
