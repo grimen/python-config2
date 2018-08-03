4d815a51c2a9fe42159620ef049ee2230202463c
Jonas Grimfelt <grimen@gmail.com>
2018-08-02 22:41:38 -0400

    bugfix: include `install_requires` in `setup.py` - to avoid install dependency issues

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 
+# =========================================
+#       IMPORTS
+# --------------------------------------
+
 import os
 import glob
 import setuptools
 
 # DISABLED/BUG: this line fails when `pip install config2` but works `pip install .`
 # from config2 import __version__
+
+
+# =========================================
+#       FUNCTIONS
+# --------------------------------------
 
 def find_data_files(data_file_patterns = [], root_path = None):
     root_path = root_path or os.path.abspath(os.path.dirname(__file__))
@@ -30,15 +39,43 @@ def find_data_files(data_file_patterns = [], root_path = None):
 
     return data_files
 
-setuptools.setup(
-    name = 'config2',
-    version = '0.1.3',
-    description = (
+def get_readme():
+    root_path = os.path.abspath(os.path.dirname(__file__))
+    readme_file_path = os.path.join(root_path, 'README.md')
+
+    with open(readme_file_path) as file:
+        readme = file.read()
+
+    return readme
+
+def get_requirements():
+    root_path = os.path.abspath(os.path.dirname(__file__))
+    requirements_file_path = os.path.join(root_path, 'requirements.txt')
+
+    with open(requirements_file_path) as file:
+        requirements = [requirement.strip() for requirement in file.readlines()]
+        requirements = filter(lambda requirement: len(requirement), requirements)
+        requirements = list(requirements)
+
+    return requirements
+
+
+# =========================================
+#       MAIN
+# --------------------------------------
+
+readme = get_readme()
+requirements = get_requirements()
+packages = setuptools.find_packages()
+data_files = find_data_files(['*.*'], os.path.join('config2', 'tests', '__fixtures__'))
+
+config = {
+    'name': 'config2',
+    'version': '0.1.3',
+    'description': (
         'Python application configuration - highly inspired by `node-config`.'
     ),
-    long_description = open('README.md').read(),
-    long_description_content_type = 'text/markdown',
-    keywords = [
+    'keywords': [
         'config',
         'configuration',
         'configurations',
@@ -50,30 +87,20 @@ setuptools.setup(
         'node-config',
         'python-config',
     ],
-    author = 'Jonas Grimfelt',
-    author_email = 'grimen@gmail.com',
-    url = 'https://github.com/grimen/python-config2',
-    download_url = 'https://github.com/grimen/python-config2',
-    project_urls = {
+    'author': 'Jonas Grimfelt',
+    'author_email': 'grimen@gmail.com',
+    'url': 'https://github.com/grimen/python-config2',
+    'download_url': 'https://github.com/grimen/python-config2',
+    'project_urls': {
         'repository': 'https://github.com/grimen/python-config2',
         'bugs': 'https://github.com/grimen/python-config2/issues',
     },
-    packages = setuptools.find_packages(),
-    package_dir = {
-        'config2': 'config2'
-    },
-    package_data = {
-        '': [
-            'MIT-LICENSE',
-            'README.md',
-        ],
-        'config2': [
-            '*.*',
-        ]
-    },
-    data_files = find_data_files(['*.yml'], 'config2/tests/__fixtures__'),
-    license = 'MIT',
-    classifiers = [
+    'license': 'MIT',
+
+    'long_description': readme,
+    'long_description_content_type': 'text/markdown',
+
+    'classifiers': [
         'Topic :: Software Development :: Libraries',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -87,7 +114,26 @@ setuptools.setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
-    zip_safe = True,
-    include_package_data = True,
-    setup_requires = ['setuptools_git >= 1.2'],
-)
+
+    'packages': packages,
+    'package_dir': {
+        'config2': 'config2',
+    },
+    'package_data': {
+        '': [
+            'MIT-LICENSE',
+            'README.md',
+        ],
+        'config2': [
+            '*.*',
+        ],
+    },
+    'data_files': data_files,
+    'include_package_data': True,
+    'zip_safe': True,
+
+    'install_requires': requirements,
+    'setup_requires': ['setuptools_git >= 1.2'],
+}
+
+setuptools.setup(**config)
