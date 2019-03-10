542d22fd78d297ccd1e8b374a1a6293f6667b37a
Jonas Grimfelt <grimen@gmail.com>
2019-03-10 04:24:19 -0400

    ci: debug

--- config2/config.py
+++ config2/config.py
@@ -112,6 +112,8 @@ class Config(AttributeDict):
         env = env or DEFAULT_ENV
         # env = env or Config.detect_env(DEFAULT_ENV_KEYS, DEFAULT_ENV)
 
+        print('DEBUG #0', path)
+
         if not path:
             try:
                 this_file_path = os.path.abspath(__file__)
@@ -131,10 +133,12 @@ class Config(AttributeDict):
                     if is_caller_file:
                         path = os.path.dirname(frame.filename)
 
+                        print('DEBUG #1', path)
+
                         break
 
+
             except Exception as error:
-                print('DEBUG `path` resolve issue', error)
                 path = os.getcwd()
 
         path = os.path.abspath(path)
